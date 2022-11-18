2072c1dc72d94e634ead1c64590c60838d657472
Gerry Hazelton <gerryhazelton@gmail.com>
2022-11-18 05:26:57 -0800

    Update 11/18/22

--- TempleVolunteerClient/ViewModels/Area/AreaViewModel.cs
+++ TempleVolunteerClient/ViewModels/Area/AreaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using TempleVolunteerClient;
 
 namespace TempleVolunteerClient
@@ -8,12 +9,24 @@ namespace TempleVolunteerClient
     public class AreaViewModel : Audit
     {
         public int AreaId { get; set; }
+
+        [Display(Name = "Name")] 
         public string Name { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Note")]
         public string? Note { get; set; }
+
+        [Display(Name = "Supply Items")]
         public ICollection<SelectListItem>? SupplyItems { get; set; }
+
         public int[]? SupplyItemIds { get; set; }
+
+        [Display(Name = "Event Tasks")]
         public ICollection<SelectListItem>? EventTasks { get; set; }
+
         public int[]? EventTaskIds { get; set; }
     }
 }
